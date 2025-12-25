8fff83d6b8a04a8de6585b3bc371622a404a2bd6
AlABarazi <alaabarazi@gmail.com>
2025-12-26 01:03:25 +0200

    Update greeting and fix items endpoint

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Hello!", "v": "0.1" }
+    return {"msg": "Hello from Ala on Render!", "v": "0.2"}
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"id": item_id, "q": q}
